1e966c44182498b7ed406a86e67f8e150dfa0822
kyonzz <nhat.hip95@gmail.com>
2017-02-21 14:25:12 +0700

    clgt

--- app/scripts/components/AllIssues.jsx
+++ app/scripts/components/AllIssues.jsx
@@ -36,7 +36,7 @@ class AllIssues extends Component {
         <div>
           {this.props.parentIssues ? this.props.parentIssues.map(issue =>
             <div key={issue.id} style={{ visibility: issue.id !== this.props.onChangeGroupId && this.props.onChangeGroupId !== null ? 'hidden' : 'visible' }} >
-              <Collapsible className="what" key={issue.id} trigger={'TASK-' + issue.id + ` > > > ` + issue.name} transitionTime={200}>
+              <Collapsible className="what" key={issue.id} trigger={<span><h5>TASK-{issue.id} > > > {issue.name}</h5> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <p  className="class">Action creation</p></span>} antiTriggerClass="class" transitionTime={200}>
                 <div className="sup">
                   <div className="newcolumn">
                     <IssueColumn filter='new' parent={issue.id} />
